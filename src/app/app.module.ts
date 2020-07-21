1e09af5727ce055a5da0d2c2620473d76bd36457
Felipe Munoz Mazur <felipemazurifms@gmail.com>
2020-07-21 05:23:04 +0000

    Added animation module,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,37 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './components/main-view/main-view.component';
-import { ResourceInformationComponent } from './components/resource-information/resource-information.component';
+import { AccordionComponent } from './components/accordion/accordion.component';
+import { AccordionItemComponent } from './components/accordion/item/item.component';
 
 @NgModule({
-  declarations: [AppComponent, MainViewComponent, ResourceInformationComponent],
+  declarations: [
+    AppComponent,
+    MainViewComponent,
+    AccordionComponent,
+    AccordionItemComponent,
+  ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     HttpClientModule,
     FormsModule,
+    NoopAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){
+  }
+}
